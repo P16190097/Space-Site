51d220db9cc805ba39b57afc8d3b045313487314
Joshua Mayo <P16190097@LEC-ADMIN.dmu.ac.uk>
2020-02-11 13:55:37 +0000

    added js sliding nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,55 @@
 console.log("hello");
 
-document.getElementId('navController').addEventListener()
+let navOpen = false;
+
+function toggleNav() {
+    if ((getWidth()) >= 768) {
+        if (navOpen) {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("main").style.marginLeft = "0";
+            document.getElementById("navController").innerHTML = '<i class="fa fa-angle-right"></i>';
+            navOpen = false;
+        }
+        else {
+            document.getElementById("mySidenav").style.width = "250px";
+            document.getElementById("main").style.marginLeft = "250px";
+            document.getElementById("navController").innerHTML = '<i class="fa fa-angle-left"></i>';
+            navOpen = true;
+        }
+    }
+    else {
+        if (navOpen) {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementsByTagName("main").style.marginLeft = "0";
+            document.getElementById("navController").innerHTML = '<i class="fa fa-angle-right"></i>';
+            navOpen = false;
+        }
+        else {
+            document.getElementById("mySidenav").style.width = "250px";
+            document.getElementsByTagName("main").style.marginLeft = "250px";
+            document.getElementById("navController").innerHTML = '<i class="fa fa-angle-left"></i>';
+            navOpen = true;
+        }
+    }
+
+}
+
+function getWidth() {
+    return Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.documentElement.clientWidth
+    );
+}
+
+function getHeight() {
+    return Math.max(
+        document.body.scrollHeight,
+        document.documentElement.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.offsetHeight,
+        document.documentElement.clientHeight
+    );
+}
