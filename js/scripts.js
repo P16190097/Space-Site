725056ba212498cddcb2ecd2d05b065fa110506f
P16190097 <p16190097@my365.dmu.ac.uk>
2020-03-16 17:11:01 +0000

    added canvas to all page headers + star script

--- js/scripts.js
+++ js/scripts.js
@@ -72,14 +72,118 @@ const performHttpGet = async (url, processResp, onFail) => {
 };
 
 /***********************************************/
-/*     nasa API url and response function      */
+/*     html5 canvas header drawing script      */
 /***********************************************/
 
-const demoUrl = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`
-const setImg = (resp) => {
-    const { url } = resp;
-    document.getElementById("img").innerHTML = `<img src="${url}" class="logo-img" />`;
+const resizeReset = () => {
+    const canvas = document.getElementById('mesh-canvas');
+    w = canvasBody.width = canvas.offsetWidth;
+    h = canvasBody.height = canvas.offsetHeight;
+};
+
+const opts = {
+    particleColor: "rgb(200,200,200)",
+    lineColor: "rgb(200,200,200)",
+    particleAmount: 10,
+    defaultSpeed: 1,
+    variantSpeed: 1,
+    defaultRadius: 2,
+    variantRadius: 2,
+    linkRadius: 200,
+};
+
+window.addEventListener("resize", () => {
+    deBouncer();
+});
+
+const deBouncer = () => {
+    clearTimeout(tid);
+    tid = setTimeout(() => {
+        resizeReset();
+    }, delay);
+};
+
+const checkDistance = (x1, y1, x2, y2) => {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+};
+
+const linkPoints = (point1, hubs) => {
+    for (let i = 0; i < hubs.length; i++) {
+        let distance = checkDistance(point1.x, point1.y, hubs[i].x, hubs[i].y);
+        let opacity = 1 - distance / opts.linkRadius;
+        if (opacity > 0) {
+            drawArea.lineWidth = 0.5;
+            drawArea.strokeStyle = `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, ${opacity})`;
+            drawArea.beginPath();
+            drawArea.moveTo(point1.x, point1.y);
+            drawArea.lineTo(hubs[i].x, hubs[i].y);
+            drawArea.closePath();
+            drawArea.stroke();
+        }
+    }
 }
 
-//performHttpGet(demoUrl, setImg);
+Particle = function (xPos, yPos) {
+    this.x = Math.random() * w;
+    this.y = Math.random() * h;
+    this.speed = opts.defaultSpeed + Math.random() * opts.variantSpeed;
+    this.directionAngle = Math.floor(Math.random() * 360);
+    this.color = opts.particleColor;
+    this.radius = opts.defaultRadius + Math.random() * opts.variantRadius;
+    this.vector = {
+        x: Math.cos(this.directionAngle) * this.speed,
+        y: Math.sin(this.directionAngle) * this.speed
+    };
+    this.update = () => {
+        this.border();
+        this.x += this.vector.x;
+        this.y += this.vector.y;
+    };
+    this.border = () => {
+        if (this.x >= w || this.x <= 0) {
+            this.vector.x *= -1;
+        }
+        if (this.y >= h || this.y <= 0) {
+            this.vector.y *= -1;
+        }
+        if (this.x > w) this.x = w;
+        if (this.y > h) this.y = h;
+        if (this.x < 0) this.x = 0;
+        if (this.y < 0) this.y = 0;
+    };
+    this.draw = () => {
+        drawArea.beginPath();
+        drawArea.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        drawArea.closePath();
+        drawArea.fillStyle = this.color;
+        drawArea.fill();
+    };
+};
 
+const setup = () => {
+    particles = [];
+    resizeReset();
+    for (let i = 0; i < opts.particleAmount; i++) {
+        particles.push(new Particle());
+    }
+    window.requestAnimationFrame(loop);
+}
+
+const loop = () => {
+    window.requestAnimationFrame(loop);
+    drawArea.clearRect(0, 0, w, h);
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].draw();
+    }
+    for (let i = 0; i < particles.length; i++) {
+        linkPoints(particles[i], particles);
+    }
+}
+
+const canvasBody = document.getElementById('mesh-canvas'),
+    drawArea = canvasBody.getContext("2d");
+let delay = 200, tid,
+    rgb = opts.lineColor.match(/\d+/g);
+resizeReset();
+setup();
